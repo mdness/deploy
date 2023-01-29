5ffbf0524b4dec59384ec0b1a1aa882965cc6c05
mdness <maitecelcastelli@gmail.com>
2023-01-29 16:21:01 -0300

    fourth commit

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ app.get('/', (req, res)=>{
     res.send('<h1>FUNCIONAAAAAAAAAAAA</h1>')
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 app.listen(PORT, () => {
     console.log(`Todo bien`)
