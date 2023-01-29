d7423cfd9dce5f214bed53505c1b8288f484219c
mdness <maitecelcastelli@gmail.com>
2023-01-29 16:13:48 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send('<h1>FUNCIONAAAAAAAAAAAA</h1>')
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`STodo bien`)
+});
